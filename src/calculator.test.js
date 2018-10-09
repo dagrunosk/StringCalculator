be0c04a2323f4456db5b99d6e34659141ec8381f
dagrunosk <dagrun17@ru.is>
2018-10-09 13:42:24 +0000

    Add for one number

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -6,3 +6,8 @@ it("should return zero on an empty string", () => {
 	expect(Add("")).toBe(0);
 });
 
+it ("should return number when only one number is in the string", () => {
+	expect(Add("5")).toBe(5);
+});
+
+
